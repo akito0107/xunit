479ddee8ae84d7fd35b610a6f3573f4720ea003c
Akito Ito <akito0107@gmail.com>
2018-05-19 16:45:16 +0900

    18-148

--- xunit.go
+++ xunit.go
@@ -4,26 +4,30 @@ import "reflect"
 
 type TestMethod func()
 
-type TestCase struct {
-	name string
-}
-
-func (t *TestCase) Run() {
-	method := reflect.ValueOf(t).MethodByName(t.name)
+func Run(t TestCase) {
+	method := reflect.ValueOf(t).MethodByName(t.Name())
 	method.Call([]reflect.Value{})
 }
 
+type TestCase interface {
+	Name() string
+}
+
 type WasRun struct {
-	TestCase
+	name   string
 	wasRun bool
 }
 
 func NewWasRun(name string) *WasRun {
 	return &WasRun{
-		TestCase: TestCase{name: name},
+		name: name,
 	}
 }
 
 func (w *WasRun) TestMethod() {
 	w.wasRun = true
+}
+
+func (w *WasRun) Name() string {
+	return w.name
 }
--- xunit_test.go
+++ xunit_test.go
@@ -8,6 +8,6 @@ import (
 func TestWasRun(t *testing.T) {
 	test := NewWasRun("TestMethod")
 	fmt.Println(test.wasRun)
-	test.Run()
+	Run(test)
 	fmt.Println(test.wasRun)
 }
