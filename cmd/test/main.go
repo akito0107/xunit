a1816fcca48ed37f8344ae44f0de27fcb2a3d31b
Akito Ito <akito0107@gmail.com>
2018-05-19 17:25:01 +0900

    154-1

--- cmd/test/main.go
+++ cmd/test/main.go
@@ -4,16 +4,24 @@ import "github.com/akito0107/xunit"
 
 type TestCaseTest struct {
 	Name string
+	test *xunit.WasRun
+}
+
+func (t *TestCaseTest) SetUp() {
+	t.test = xunit.NewWasRun("TestMethod")
 }
 
 func (t *TestCaseTest) TestRunning() {
-	test := xunit.NewWasRun("TestMethod")
-	xunit.AssertNot(test.WasRun)
-	xunit.Run(test)
-	xunit.Assert(test.WasRun)
+	xunit.Run(t.test)
+	xunit.Assert(t.test.WasRun)
+}
+
+func (t *TestCaseTest) TestSetUp() {
+	xunit.Run(t.test)
+	xunit.Assert(t.test.WasSetUp)
 }
 
 func main() {
-	t1 := &TestCaseTest{"TestRunning"}
-	xunit.Run(t1)
+	xunit.Run(&TestCaseTest{Name: "TestRunning"})
+	xunit.Run(&TestCaseTest{Name: "TestSetUp"})
 }
--- xunit.go
+++ xunit.go
@@ -3,26 +3,37 @@ package xunit
 import "reflect"
 
 func Run(i interface{}) {
+	setupper, ok := i.(SetUpper)
+	if ok {
+		setupper.SetUp()
+	}
+
 	n := reflect.ValueOf(i).Elem().FieldByName("Name").Interface()
 	name, _ := n.(string)
 	method := reflect.ValueOf(i).MethodByName(name)
 	method.Call([]reflect.Value{})
 }
 
-type TestCase interface {
-	Name() string
+type SetUpper interface {
+	SetUp()
 }
 
 type WasRun struct {
-	Name   string
-	WasRun bool
+	Name     string
+	WasRun   bool
+	WasSetUp bool
 }
 
 func NewWasRun(name string) *WasRun {
 	return &WasRun{
-		Name:   name,
-		WasRun: false,
+		Name:     name,
+		WasRun:   false,
+		WasSetUp: false,
 	}
+}
+
+func (w *WasRun) SetUp() {
+	w.WasSetUp = true
 }
 
 func (w *WasRun) TestMethod() {
